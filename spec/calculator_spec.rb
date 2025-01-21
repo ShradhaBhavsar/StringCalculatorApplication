70551e7ecb6833a474e70a4420c6e6a8db5bd9f2
SradhaBhavsar <shradha.bhavsar@deeporion.com>
2025-01-21 23:18:26 +0530

    Added some positive scenarios test cases for simple inputs

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,5 +4,17 @@ require 'rspec'
 
 RSpec.describe Calculator do
   describe '#add' do
+    it 'returns 0 for an empty string' do
+      expect(Calculator.add("")).to eq(0)
+    end
+    it 'returns the number for a single number' do
+      expect(Calculator.add("1")).to eq(1)
+    end
+    it 'returns the sum for two numbers' do
+      expect(Calculator.add("1,5")).to eq(6)
+    end
+    it 'returns the sum for any amount of numbers' do
+      expect(Calculator.add("1,2,3,4")).to eq(10)
+    end
   end
 end
