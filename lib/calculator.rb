d0903c952a6dd738501e696dc539755c485055d1
SradhaBhavsar <shradha.bhavsar@deeporion.com>
2025-01-21 23:34:44 +0530

    Added logic to extract delimeters and numbers

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,6 +1,19 @@
 class Calculator
-    def self.add(numbers)
-      #TODO addition logic
+  def self.add(numbers)
+    return 0 if numbers.empty?
+
+    delimiter, numbers = extract_delimiter_and_numbers(numbers)
+  end
+
+  private
+
+  def self.extract_delimiter_and_numbers(numbers)
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2..])
+      [delimiter, parts[1]]
+    else
+      [",", numbers]
     end
   end
 end
